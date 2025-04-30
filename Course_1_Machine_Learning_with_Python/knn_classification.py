5d1d16a1958c7ae1ec89a15c8e8a912f63bc0743
mennosmit-dev <mennosjoerd@hotmail.com>
2025-04-30 12:46:54 +0200

    Update knn_classification.py

--- Course_1_Machine_Learning_with_Python/knn_classification.py
+++ Course_1_Machine_Learning_with_Python/knn_classification.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 # %matplotlib inline
-
+"""
 Imagine a telecommunications provider has segmented its customer base by service usage patterns, categorizing the customers into four groups. If demographic data can be used to predict group membership, the company can customize offers for individual prospective customers. It is a classification problem. That is, given the dataset,  with predefined labels, we need to build a model to be used to predict class of a new or unknown case.
 
 The example focuses on using demographic data, such as region, age, and marital, to predict usage patterns.
