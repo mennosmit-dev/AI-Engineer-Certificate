8b8092261a31630b5a7ecc0549e460a55e8ecc4e
mennosmit-dev <mennosjoerd@hotmail.com>
2025-08-12 16:54:02 +0200

    Update baby_bert.py

--- 09_Generative_AI_Language_Modeling_with_Transformers/baby_bert.py
+++ 09_Generative_AI_Language_Modeling_with_Transformers/baby_bert.py
@@ -1,4 +1,4 @@
-""
+"""
 Objectives
 - **Demystify tokenization**: Introduce the critical first step in processing text for Natural Language Processing (NLP) — tokenization. We'll learn how to convert raw text into a format that's amenable to machine processing.
 - **Decode encoder models**: Shed light on the structure and function of encoder models, particularly how they capture and process the complexities of language.
