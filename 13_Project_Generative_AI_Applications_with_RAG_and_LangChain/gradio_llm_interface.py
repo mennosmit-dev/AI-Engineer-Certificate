aadc7ded62d3ed2d8a4aca89c5c589660c8eb42c
mennosmit-dev <mennosjoerd@hotmail.com>
2025-08-31 17:55:34 +0200

    Update gradio_llm_interface.py

--- 13_Project_Generative_AI_Applications_with_RAG_and_LangChain/gradio_llm_interface.py
+++ 13_Project_Generative_AI_Applications_with_RAG_and_LangChain/gradio_llm_interface.py
@@ -1,5 +1,187 @@
+"""Objectives
+- Use Gradio to build interactive front-end interfaces, enabling users to interact with backend LLMs seamlessly
+- Create a functional chatbot, allowing users to input queries and receive responses from an LLM
+- Implement essential and commonly used Gradio elements, such as text input fields, buttons, and display areas, to enhance the users' experience
+- Customize and deploy web-based applications, facilitating various use cases including customer support, data analysis, and others
 
+Installing required libraries
+"""
 
 pip install virtualenv 
 virtualenv my_env # create a virtual environment named my_env
 source my_env/bin/activate # activate my_env
+
+# installing necessary pacakges in my_env
+python3.11 -m pip install \
+gradio==4.44.0 \
+pydantic==2.10.6 \
+ibm-watsonx-ai==1.1.2 \
+langchain==0.2.11 \
+langchain-community==0.2.10 \
+langchain-ibm==0.1.11
+
+import gradio as gr
+
+def add_numbers(Num1, Num2):
+    return Num1 + Num2
+
+# Define the interface
+demo = gr.Interface(
+    fn=add_numbers, 
+    inputs=[gr.Number(), gr.Number()], # Create two numerical input fields where users can enter numbers
+    outputs=gr.Number() # Create numerical output fields
+)
+
+# Launch the interface
+demo.launch(server_name="127.0.0.1", server_port= 7860)
+
+python3.11 gradio_demo.py
+
+"""
+Exercise
+
+Can you create a Gradio application that can combine two input sentences together? Use what you know from the demo and your Python knowledge to create this app. Take your time to complete this exercise.
+"""
+import gradio as gr
+
+def combine(a, b):
+    return a + " " + b
+
+demo = gr.Interface(
+    fn=combine,
+    inputs = [
+        gr.Textbox(label="Input 1"),
+        gr.Textbox(label="Input 2")
+    ],
+    outputs = gr.Textbox(label="Output")
+)
+demo.launch(server_name="127.0.0.1", server_port= 7860)
+
+import gradio as gr
+
+def sentence_builder(quantity, tech_worker_type, countries, place, activity_list, morning):
+    return f"""The {quantity} {tech_worker_type}s from {" and ".join(countries)} went to the {place} where they {" and ".join(activity_list)} until the {"morning" if morning else "night"}"""
+
+"""
+Let's have a look at an example of some of Gradio's common input types.
+"""
+demo = gr.Interface(
+    fn=sentence_builder,
+    inputs=[
+        gr.Slider(3, 20, value=4, step=1, label="Count", info="Choose between 3 and 20"),
+        gr.Dropdown(
+            ["Data Scientist", "Software Developer", "Software Engineer"], 
+            label="tech_worker_type", 
+            info="Will add more tech worker types later!"
+        ),
+        gr.CheckboxGroup(["Canada", "Japan", "France"], label="Countries", info="Where are they from?"),
+        gr.Radio(["office", "restaurant", "meeting room"], label="Location", info="Where did they go?"),
+        gr.Dropdown(
+            ["partied", "brainstormed", "coded", "fixed bugs"], 
+            value=["brainstormed", "fixed bugs"], 
+            multiselect=True, 
+            label="Activities", 
+            info="Which activities did they perform?"
+        ),
+        gr.Checkbox(label="Morning", info="Did they do it in the morning?"),
+    ],
+    outputs="text",
+    examples=[
+        [3, "Software Developer", ["Canada", "Japan"], "restaurant", ["coded", "fixed bugs"], True],
+        [4, "Data Scientist", ["Japan"], "office", ["brainstormed", "partied"], False],
+        [10, "Software Engineer", ["Canada", "France"], "meeting room", ["brainstormed"], False],
+        [8, "Data Scientist", ["France"], "restaurant", ["coded"], True],
+    ]
+)
+
+demo.launch(server_name="127.0.0.1", server_port= 7860)
+
+python3.11 common_input_types.py
+
+"""
+Create a Q&A bot
+"""
+# Import the necessary packages
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai import Credentials
+from langchain_ibm import WatsonxLLM
+
+# Specify the model and project settings 
+# (make sure the model you wish to use is commented out, and other models are commented)
+#model_id = 'mistralai/mixtral-8x7b-instruct-v01' # Specify the Mixtral 8x7B model
+model_id = 'ibm/granite-3-3-8b-instruct' # Specify IBM's Granite 3.3 8B model
+
+# Set the necessary parameters
+parameters = {
+    GenParams.MAX_NEW_TOKENS: 256,  # Specify the max tokens you want to generate
+    GenParams.TEMPERATURE: 0.5, # This randomness or creativity of the model's responses
+}
+
+project_id = "skills-network"
+
+# Wrap up the model into WatsonxLLM inference
+watsonx_llm = WatsonxLLM(
+    model_id=model_id,
+    url="https://us-south.ml.cloud.ibm.com",
+    project_id=project_id,
+    params=parameters,
+)
+
+# Get the query from the user input
+query = input("Please enter your query: ")
+
+# Print the generated response
+print(watsonx_llm.invoke(query))
+
+python3.11 simple_llm.py
+
+"""
+Integrate the application into Gradio
+"""
+# Import necessary packages
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai import Credentials
+from langchain_ibm import WatsonxLLM
+import gradio as gr
+
+# Model and project settings
+model_id = 'mistralai/mixtral-8x7b-instruct-v01' # Specify the Mixtral 8x7B model
+#model_id = 'ibm/granite-3-3-8b-instruct' # Specify IBM's Granite 3.3 8B model
+
+# Set necessary parameters
+parameters = {
+    GenParams.MAX_NEW_TOKENS: 256,  # Specify the max tokens you want to generate
+    GenParams.TEMPERATURE: 0.5, # This randomness or creativity of the model's responses
+}
+
+project_id = "skills-network"
+
+# Wrap up the model into WatsonxLLM inference
+watsonx_llm = WatsonxLLM(
+    model_id=model_id,
+    url="https://us-south.ml.cloud.ibm.com",
+    project_id=project_id,
+    params=parameters,
+)
+
+# Function to generate a response from the model
+def generate_response(prompt_txt):
+    generated_response = watsonx_llm.invoke(prompt_txt)
+    return generated_response
+
+# Create Gradio interface
+chat_application = gr.Interface(
+    fn=generate_response,
+    allow_flagging="never",
+    inputs=gr.Textbox(label="Input", lines=2, placeholder="Type your question here..."),
+    outputs=gr.Textbox(label="Output"),
+    title="Watsonx.ai Chatbot",
+    description="Ask any question and the chatbot will try to answer."
+)
+
+# Launch the app
+chat_application.launch(server_name="127.0.0.1", server_port= 7860)
+
+python3.11 llm_chat.py
